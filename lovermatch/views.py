410b20168ab487c840abb41a3ef96c44475b9b4d
liutianyi <skywalker_lty@163.com>
2016-11-01 21:04:38 +0800

    add some codes for algorithm

--- lovermatch/views.py
+++ lovermatch/views.py
@@ -11,6 +11,7 @@ from lovermatch.models import User
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
+from operator import itemgetter
 
 
 # Create your views here.
@@ -87,3 +88,55 @@ def login(req):
 def results(request, username):
     context = {'username': username}
     return render(request, 'lovermatch/signup_results.html', context)
+
+
+def match(request, usr):
+    """match algorithm for usr to find all users who are similar to him/her based on features and weights"""
+    user = User.objects(user=usr)
+    features_to_match = request.POST['features_to_match']
+    weights = request.POST['weights']
+    n = request.POST['amount']
+    matchlist = {}
+    for current_user in User.objects:
+        sim = get_similarity(user, current_user, features_to_match, weights)
+        matchlist[current_user] = sim
+    sorted_matchlist = sorted(matchlist.items(), key=itemgetter(1), reverse=True)[0:n]
+    context = {'sortedMatchList': sorted_matchlist}
+    # return response
+
+
+def height_similarity(h1, h2):
+    """h1 is male's height, h2 is female's height"""
+    gap = h1 - h2
+    if gap < 0:
+        return 0.5
+    elif 0 <= gap and gap < 10:
+        return 0.8
+    elif 10 <= gap and gap < 20:
+        return 0.9
+    else:
+        return 1.0
+
+
+def hometown_similarity(hid1, hid2):
+    if hid1 == hid2:
+        return 1.0
+
+
+def get_similarity(u1, u2, features_to_match, weights):
+    value = 0.0
+    for i in range(len(features_to_match)):
+        feature = features_to_match[i]
+        weight = weights[i]
+        if feature == 'height':
+            if u1.gender == 0 and u2.gender == 1:
+                value += weight * height_similarity(u1.height, u2.height)
+            else:
+                value += weight * height_similarity(u2.height, u1.height)
+        elif feature == 'hometownId':
+            value += weight * hometown_similarity(u1.hometownId, u2.hometownId)
+        # elif feature == blabla
+
+    value = value / sum(weights)
+    return value
+
