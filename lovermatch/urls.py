6a5b268ed0be3f2edacaec7e524a8e12de1498bf
LiuTianyi <skywalker_lty@163.com>
2016-11-06 11:34:30 +0800

    update codes for urls.py

--- lovermatch/urls.py
+++ lovermatch/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # url(r'^signup_form/$', views.show_signup_form, name='show_signup_form'),
     url(r'^signup_form', views.show_signup_form, name='show_signup_form'),
     url(r'^signup/$', views.signup, name='signup'),
-    url('^activate/(?P<token>[A-Za-z0-9-_.]*)/$', views.active_user, name='active_user'),
+    url(r'^activate/(?P<token>\w+.[-_\w]*\w+.[-_\w]*\w+)/$', views.active_user, name='active_user'),
     # url(r'^(?P<username>[\w]+)/results/$', views.results, name='results'),
     url(r'^login', views.login),
     url(r'^show', views.showInfo),
