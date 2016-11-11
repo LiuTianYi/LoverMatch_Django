256b599b6725e770f69ad0ce45a751d062620cdb
Leechung <tongleechung86@gmail.com>
2016-11-11 13:50:49 +0800

    front end

--- templates/front/js/index.js
+++ templates/front/js/index.js
@@ -46,10 +46,7 @@ $(function() {
     $("#nav-match").css({"background-color":"#f8f8f8"});
     $("#nav-matched").css({"background-color":"#e7e7e7"});
 
-    $.post('http://localhost:8018/login', {} ,function(data){
-      alert(data);
 
-    }, "json");
 
   });
   
@@ -57,11 +54,7 @@ $(function() {
     //alert("change color");
     $("#nav-matched").css({"background-color":"#f8f8f8"});
     $("#nav-match").css({"background-color":"#e7e7e7"});
-    $.post('http://localhost:8018/matchlist2',function(data){
-      //alert(data);
-      
 
-    });
   });
 });
 
@@ -73,7 +66,7 @@ function getmatch(){
     url: 'http://168.63.205.250/match/',  //这里是你的api名字
     dataType: "json", // type of returned data
     success: function(data) { // if ajax function results success 这里返回你后台检查通过或者不通过的信息
-    alert(data){function(){matchli = data}},
+    alert(data){function(){matchli = data}}
     }
   });
 }
