fbba5d4e9ed1c9d0a254592393a1548ff31150d4
Leechung <tongleechung86@gmail.com>
2016-11-28 22:55:14 +0800

    lee

--- templates/front/js/index.js
+++ templates/front/js/index.js
@@ -93,7 +93,7 @@ function getmatch(){
         if (data["lovermatch"][i][0]["gradeId"] != null){
         	infostring.concat("<p>他现在在上"+grade["grade"][parseInt(data["lovermatch"][i][0]["gradeId"])]+".</p>");
         }
-      	$("#match_spot").append('<li class="list-group-item item" id="hover-parent"><span class="badge">'+String(parseFloat(data["lovermatch"][i][1])*100)+'</span>'+data["lovermatch"][i][0]['name']+'<div class="match-avt" style="background-image: url('+data["lovermatch"][i][0]['photoAddress']+')"></div><div id="hc"><p>他的身高是163cm</p><p>他的体重是53kg</p></div><div class="progress bar"><div class="progress-bar" role="progressbar" aria-valuenow="'+String(parseFloat(data["lovermatch"][i][1])*100)+'" aria-valuemin="0" aria-valuemax="100" style="width: '+String(parseFloat(data["lovermatch"][i][1])*100)+'%;"></div></li>')
+      	$("#match_spot").append('<li class="list-group-item item" id="hover-parent"><span class="badge">'+String(parseFloat(data["lovermatch"][i][1])*100)+'</span>'+data["lovermatch"][i][0]['name']+'<div class="match-avt" style="background-image: url('+data["lovermatch"][i][0]['photoAddress']+')"></div><div id="hc">'+infostring+'</div><div class="progress bar"><div class="progress-bar" role="progressbar" aria-valuenow="'+String(parseFloat(data["lovermatch"][i][1])*100)+'" aria-valuemin="0" aria-valuemax="100" style="width: '+String(parseFloat(data["lovermatch"][i][1])*100)+'%;"></div></li>')
     }
   });
 }
